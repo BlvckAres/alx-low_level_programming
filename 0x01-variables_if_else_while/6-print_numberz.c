b173a6e31ee3fe82000e79ed03d150ffa72d2bef
Xolani <xzchauke@proton.me>
2022-07-02 06:27:57 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry point
+ *
+ * Result: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = '0'; numbers <= '9'; numbers++)
+		putchar(numbers);
+	
+	putchar('\n');
+	
+	return (0);
+}
