91a2eb6040c5c42fe52049b4fede2c9358509135
Xolani <xzchauke@proton.me>
2022-07-03 04:07:54 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Write a programme that prints possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numba;
+
+	for (numba = '0'; numba <= '9'; numba++)
+	{
+		putchar(numba);
+		if (numba != '9')
+			putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
