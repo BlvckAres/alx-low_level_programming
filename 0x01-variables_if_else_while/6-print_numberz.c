3451d5128c199c00b5d2066992b68a7b2d0c9904
Xolani <xzchauke@proton.me>
2022-07-03 03:00:03 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
- * Description: Print single digit numbers of base 10 starting from 0
+ * main - Print single digit numbers of base 10 starting from 0,
  * A new line
  * You are not allowed to use any variable of type char
  * You can only use the putchar function
  * Every other function (printf, puts, etc are prohibited)
  * You can only use putchar twice in your code
- * Result: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
