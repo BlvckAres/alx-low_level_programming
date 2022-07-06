acad42b112ad524a9711b36bbedb1eaf8fc4ff10
Xolani <xzchauke@proton.me>
2022-07-06 04:59:09 -0700

    Task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10.c - a function that prints 10 times the alphabet.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char j;
+
+	while (count++ <= 9)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		_putchar('\n');
+		}
+	}
+}
