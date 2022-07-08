1b6a7fee40966ac83838962fa9cc980f9ba65abd
Xolani <xzchauke@proton.me>
2022-07-07 20:30:04 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,7 @@
 /**
  * print_square - a function that prints a square, followed by a new line.
  * @size: size of both width and length
- * Return: a square made
+ * Return: Always 0
  */
 void print_square(int size)
 {
