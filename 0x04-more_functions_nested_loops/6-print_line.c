7558add57f50969e9ca769ceb901d37d0e57a107
Xolani <xzchauke@proton.me>
2022-07-07 19:06:39 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,6 +3,7 @@
 /**
  * print_line - draws a straight line using the character _.
  * @n: The number of _ character to be printed.
+ * Return: the line for the n value
  */
 void print_line(int n)
 {
