8afa5fdd56882977f0baf6525b4ab8401a48c778
Xolani <xzchauke@proton.me>
2022-07-14 09:09:03 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,12 +10,12 @@ include "main.h"
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int x = 0, dest_y = 0;
+	int index = 0, dest_y = 0;
 
-	while (dest[x++])
+	while (dest[index++])
 		dest_y++;
 
-	for (x = 0; src[x] && x < n; x++)
-		dest[dest_y++] = src[x];
+	for (index = 0; src[index] && x < n; index++)
+		dest[dest_y++] = src[index];
 	return (dest);
 }
