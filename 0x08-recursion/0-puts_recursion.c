4d2c297cf2295288f28499eae02cd965ed0d3e79
Xolani <xzchauke@proton.me>
2022-07-20 04:10:11 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
+		_putchar('\0');
+	else
 	{
 		_putchar(*s);
-		_puts_recurion(s + 1);
+		_putchar_recurison(s + 1);
 	}
-	else
-		_putchar('\n');
 }
