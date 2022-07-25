27fcff7c9f213611c075c25184d7415fe3f4e6a0
Xolani <xzchauke@proton.me>
2022-07-25 10:14:12 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
  * main - A program that prints the number of arguments passed into it.
  * @argc: The number of arguments supplied to the program.
@@ -7,7 +7,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d\n", argc - 1);
 
