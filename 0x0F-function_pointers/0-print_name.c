d04051608ec32d88e9004069998577c07575c100
Xolani <xzchauke@proton.me>
2022-08-04 00:11:54 -0700

    task_0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,12 @@
  * print_name - a function that prints a name.
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name.
+ *
+ * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-		f(name);
+	f(name);
 
 }
