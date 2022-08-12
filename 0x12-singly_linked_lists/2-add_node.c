92a35ebacca6c4235f4de40a0cfeaca858d82fa8
Xolani <xzchauke@proton.me>
2022-08-11 22:30:03 -0700

    updated functions.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,22 +9,29 @@
   */
 list_t *add_node(list_t **head, const char *str)
 {
+	char *alx;
+	int len;
 	list_t *new;
-	size_t x;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	new->str = strdup(str);
-	if (new->str == NULL)
+
+	alx = stralx(str);
+	if (alx == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
 
-	for (x = 0; new->str[x] != '\0'; x++);
-	new->len = x;
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = alx;
+	new->len = len;
 	new->next = *head;
+
 	*head = new;
-	return (*head);
+
+	return;
 }
