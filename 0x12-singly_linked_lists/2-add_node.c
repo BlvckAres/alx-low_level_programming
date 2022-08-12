73a56b5da6aa0bffcbd5c1bd15ce89213f2c3e83
Xolani <xzchauke@proton.me>
2022-08-11 22:42:12 -0700

    updated function.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,7 @@
   */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *alx;
+	char *dup;
 	int len;
 	list_t *new;
 
@@ -17,8 +17,8 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 
-	alx = stralx(str);
-	if (alx == NULL)
+	dup = strdup(str);
+	if (dup == NULL)
 	{
 		free(new);
 		return (NULL);
@@ -27,7 +27,7 @@ list_t *add_node(list_t **head, const char *str)
 	for (len = 0; str[len];)
 		len++;
 
-	new->str = alx;
+	new->str = dup;
 	new->len = len;
 	new->next = *head;
 
