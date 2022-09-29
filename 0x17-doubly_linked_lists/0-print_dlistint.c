a6ac89682864c254e363b4b796d414dc97195743
BlvckAres <xzchauke@proton.me>
2022-09-29 20:18:13 +0000

    adjusted function

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,15 @@ size_t print_dlistint(count dlistint_t *h)
 {
     size_t nodes = 0;
 
+    if (!h)
+	return (0);
+
+
     while (h)
     {
-        nodes++;
         printf("%d\n", h->n);
         h = h->next;
+	nodes++;
     }
 
     return (nodes);
